fd4828f346546e0b1737bd9a111b897ea8288dfc
niooii <wangh3@bxscience.edu>
2024-01-25 20:02:34 -0500

    to be or not to be

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,12 +2,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>
-        hooray its working
+    <main className="flex min-h-screen flex-col p-24">
+      <h1 className="text-9xl">
+        To be, or not to be
       </h1>
-      <p>
-        yes.
+      <p className="text-6xl p-10">
+        That is the question
       </p>
     </main>
   )
